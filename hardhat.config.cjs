c80c466c4063262c7d4442e276c149cad54301c3
fionaaboud <fiona@fionaaboud.com>
2026-02-28 18:58:30 -0500

    feat: add Monad testnet config and NFT smart contract

---
+++ hardhat.config.cjs
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      evmVersion: "cancun"
+    }
+  },
+  networks: {
+    monad: {
+      url: "https://testnet-rpc.monad.xyz",
+      chainId: 10143,
+      accounts: [process.env.MONAD_PRIVATE_KEY]
+    }
+  }
+};
